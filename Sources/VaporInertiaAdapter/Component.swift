28e2e2bf03b3a574e416072de5cc9a515d867209
David Llop <d.lloople@icloud.com>
2020-11-07 23:59:00 +0100

    Test component behaviour. Closes #1

--- Sources/VaporInertiaAdapter/Component.swift
+++ Sources/VaporInertiaAdapter/Component.swift
@@ -1,18 +1,31 @@
 import Foundation
 
-public class Component: JSONSerialization {
+public class Component {
     
     let name: String
     var properties: [String:Any]
     
-    public init(name: String, properties: [String:Any]) {
+    public init(name: String, properties: [String:Any] = [:]) {
         self.name = name
         self.properties = properties
     }
     
-    public func with(name: String, property: Any) -> Self {
-        self.properties[name] = property
-        
-        return self
+    public func with(key: String, value: Any) {
+        self.properties[key] = value
+    }
+    
+    public func getName() -> String {
+        return self.name
+    }
+    
+    public func getProperties() -> [String:Any] {
+        return self.properties
+    }
+    
+    public func toJson() throws -> Any {
+        return [
+            "name": self.name,
+            "properties": self.properties
+        ]
     }
 }
---
+++ Tests/VaporInertiaAdapterTests/ComponentTest.swift
@@ -0,0 +1,55 @@
+import XCTest
+import VaporInertiaAdapter
+
+class ComponentTest: XCTestCase {
+    
+    func testComponentCanBeDeclaredWithAnyProperty() {
+        let c = self.createComponent()
+        
+        XCTAssertEqual(c.getName(), "FirstComponent")
+        XCTAssertEqual(c.getProperties().count, 3)
+    }
+    
+    func testComponentCanBeConvertedToJson() throws {
+        let component = self.createComponent()
+        
+        let json = try component.toJson() as? [String: AnyObject]
+        let properties = json?["properties"] as? [String: AnyObject]
+        
+        XCTAssertEqual(json?["name"] as? String, "FirstComponent")
+        
+        XCTAssertEqual(properties?["name"] as? String, "John")
+        XCTAssertEqual(properties?["age"] as? Int, 34)
+        XCTAssertEqual(properties?["alive"] as? Bool, true)
+    }
+    
+
+    func testPropertiesCanBeAddedToComponent() throws {
+        let component = Component(name: "FirstComponent")
+
+        XCTAssertEqual(component.getProperties().count, 0)
+
+        component.with(key: "name", value: "Alfred")
+
+        XCTAssertEqual(component.getProperties().count, 1)
+        
+        let json = try component.toJson() as? [String: AnyObject]
+        let properties = json?["properties"] as? [String: AnyObject]
+        
+        XCTAssertEqual(properties?["name"] as? String, "Alfred")
+
+
+    }
+    
+    private func createComponent() -> Component {
+        return Component(
+            name: "FirstComponent",
+            properties: [
+                "name": "John",
+                "age": 34,
+                "alive": true
+            ]
+        )
+    }
+    
+}
