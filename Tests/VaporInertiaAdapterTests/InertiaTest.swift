70faaff82b7dc5e4dba63840158e6065b7cd0176
David Llop <d.lloople@icloud.com>
2020-11-08 12:03:01 +0100

    Test inertia singleton version and container methods

--- Tests/VaporInertiaAdapterTests/InertiaTest.swift
+++ Tests/VaporInertiaAdapterTests/InertiaTest.swift
@@ -11,4 +11,19 @@ class InertiaTest: XCTestCase {
         XCTAssertTrue(inertia === another)
     }
     
+    func testVersionCanBeSetFromOutside() {
+        XCTAssertFalse(Inertia.instance().version == "my-version")
+        
+        Inertia.instance().version = "my-version"
+        
+        XCTAssertTrue(Inertia.instance().version == "my-version")
+    }
+    
+    func testContainerCanBeRenderer() throws {
+        let component = Component(name: "Event/Show", properties: ["name": "Climate Change Global Action"])
+        let content = try JSONSerialization.data(withJSONObject: component.toJson())
+        let container = Inertia.instance().container(content:content)
+        
+        XCTAssertEqual(container, "<div id='app' data-page='\(content)'></div>")
+    }
 }
