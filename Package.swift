970c742cac46bfadc82fb87f0b47a17c2c81deea
David Llop <d.lloople@icloud.com>
2020-11-14 22:26:45 +0100

    Fix some issues with Package.swift

--- Package.swift
+++ Package.swift
@@ -1,38 +1,26 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "VaporInertiaAdapter",
+    name: "vapor-inertia-adapter",
     platforms: [
         .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "VaporInertiaAdapter",
-            targets: ["VaporInertiaAdapter"]),
+        .library(name: "VaporInertiaAdapter", targets: ["VaporInertiaAdapter"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-tau"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "VaporInertiaAdapter",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "Leaf", package: "leaf"),
-            ]),
-        .testTarget(
-            name: "VaporInertiaAdapterTests",
-            dependencies: [
-                "VaporInertiaAdapter",
-                .product(name: "XCTVapor", package: "vapor"),
-            ]),
+        .target(name: "VaporInertiaAdapter", dependencies: [
+            .product(name: "Vapor", package: "vapor"),
+            .product(name: "Leaf", package: "leaf"),
+        ]),
+        .testTarget(name: "VaporInertiaAdapterTests", dependencies: [
+            .target(name: "VaporInertiaAdapter"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ]),
     ]
 )
